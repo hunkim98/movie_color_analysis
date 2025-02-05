f03c4097ed651d9183dd8bae083647c421c4fb86
Hun Kim <hunkim98@gmail.com>
2025-02-04 23:28:29 -0500

    Create correct version

--- create_strip.py
+++ create_strip.py
@@ -16,7 +16,7 @@ def create_image_strip():
         "llm",
     ]
 
-    width_cnt = 12  # each palette image will have 4 x 4 = 16 blocks
+    width_cnt = 30  # each palette image will have 4 x 4 = 16 blocks
     height_cnt = 0
 
     movies_frames_cnt = {}
@@ -44,7 +44,9 @@ def create_image_strip():
             # now we should // width_cnt to get the number of rows
         rows = math.floor(movie_total_frames / width_cnt)
         movies_frames_cnt[mov] = rows * width_cnt
+        # print(mov, rows * width_cnt)
         height_cnt += rows
+        print(height_cnt * 4)
 
     # since we have a 4 x 4 grid we should create an image of width_cnt * 4 and height_cnt * 4
     total_width = width_cnt * 4
@@ -54,51 +56,67 @@ def create_image_strip():
 
     im_height = strip_image.height
     im_width = strip_image.width
-    print("This is image", im_height, im_width)
-    agg_img_cnt = 0
+    print()
+    print("This is image", im_width, "x", im_height)
+    finished_movies = []
     for i in range(len(movies)):
         # now we need to fill in the strip image
         mov = movies[i]
         main_dir = f"./output/videos/{mov}/frames"
         folders = os.listdir(main_dir)
 
+        row_start_y = sum(
+            [int((movies_frames_cnt[m] / width_cnt)) * 4 for m in finished_movies]
+        )
+
+        print(row_start_y)
+
+        # print(mov, row_start_y)
+
+        img_cnt = 0
         for folder in folders:
             folder_path = os.path.join(main_dir, folder)
             # check if it is a directory
             if not os.path.isdir(folder_path):
                 continue
-            img_cnt = 0
             max_cnt = movies_frames_cnt[mov]
             if img_cnt >= max_cnt:
-                print("brak")
+                print("break")
+
                 break
             for fname in os.listdir(folder_path):
                 chunk_num = folder_path.split("_")[-1]
-
+                if img_cnt >= max_cnt:
+                    print("break")
+                    break
                 if fname.lower().endswith(".jpg") and fname.startswith("palette_"):
                     frame_num = fname.split("_")[-1].split(".")[0]
                     path = os.path.join(folder_path, fname)
                     img = Image.open(path)
                     img_4x4 = img.resize((4, 4))
                     # get the position to paste the image
-                    top_left_y = math.floor(agg_img_cnt / width_cnt) * 4
-                    top_left_x = (agg_img_cnt % width_cnt) * 4
-                    for i in range(4):
-                        y = top_left_y + i
-                        for j in range(4):
-                            color = img_4x4.getpixel((i, j))
+                    top_left_y = row_start_y + (img_cnt // width_cnt) * 4
+                    top_left_x = (img_cnt % width_cnt) * 4
+                    for y_i in range(4):
+                        y = top_left_y + y_i
+                        for x_i in range(4):
+                            x = top_left_x + x_i
+                            color = img_4x4.getpixel((x_i, y_i))
                             # print(color)
-                            x = top_left_x + j
                             # print(color)
                             # strip_image.putpixel((x, y), color)
                             # print(x, y, color)
                             try:
                                 strip_image.putpixel((x, y), color)
                             except:
-                                print(x, y)
+                                raise Exception(f"Error at {x}, {y} for {mov}")
                             color_index += 1
                     img_cnt += 1
-                    agg_img_cnt += 1
+        finished_movies.append(mov)
+        if i > 0:
+            # draw a line
+            for x in range(im_width):
+                strip_image.putpixel((x, row_start_y), (255, 0, 0, 10))
 
     # enlarge the image
     scale_size = 50
